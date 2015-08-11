571acc77513991cd88d47296d160369bc01825f7
NMigliorisi <migz517@fullsail.edu>
2015-08-11 12:06:13 -0400

    adding prompts

--- Migliorisi_Nicholas_Expressions_Assignment/js/Migliorisi_Nicholas_Expressions_Assignment.js
+++ Migliorisi_Nicholas_Expressions_Assignment/js/Migliorisi_Nicholas_Expressions_Assignment.js
@@ -1,1 +1,5 @@
+//Nicholas Migliorisi Expressions Assignment SDI August 2015 WDDBS
 
+var scoreOne = prompt("We are trying to find the average score of your teams basket ball team.\n Please enter a score:"); //collecting the first score
+var scoreTwo = prompt("Please enter another score:"); //collecting the second score
+var scoreThree = prompt("And another score:"); //collection the third score
