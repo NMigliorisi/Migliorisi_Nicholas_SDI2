dab06696525135170299de2b4aa2b2f252603ca4
NMigliorisi <migz517@fullsail.edu>
2015-08-12 11:23:49 -0400

    fixed prompts

--- Migliorisi_Nicholas_Expressions_Assignment/js/Migliorisi_Nicholas_Expressions_Assignment.js
+++ Migliorisi_Nicholas_Expressions_Assignment/js/Migliorisi_Nicholas_Expressions_Assignment.js
@@ -1,9 +1,18 @@
 //Nicholas Migliorisi Expressions Assignment SDI August 2015 WDDBS
 
-var scoreOne = prompt("We are trying to find the average score of your teams basket ball team.\n Please enter a score:"); //collecting the first score
-var scoreTwo = prompt("Please enter another score:"); //collecting the second score
-var scoreThree = prompt("And another score:"); //collection the third score
+var scoreOne = Number(prompt("We are trying to find the average score of your teams basket ball team.\n Please enter a score:")); //collecting the first score
+var scoreTwo = Number(prompt("Please enter another score:")); //collecting the second score
+var scoreThree = Number(prompt("And another score:")); //collection the third score
 
 var scoreTotal = scoreOne + scoreTwo + scoreThree;  //adding together prompt totals
 
-var scoreAverage = scoreTotal / 3 //finding the average of the scores
+var scoreAverage = scoreTotal / 3; //finding the average of the scores
+
+alert("Your teams average is " + scoreAverage + " points!"); //announcing the average score to the user
+
+console.log(scoreOne); //displaying score one in the console
+console.log(scoreTwo); //displaying score two in the console
+console.log(scoreThree);//displaying score three in the console
+console.log("These are the scores. Below is the total and the average"); //telling the user what they are seeing in the console
+console.log(scoreTotal);//displaying the total of the scores in the console
+console.log(scoreAverage);//displaying the average score in the console
