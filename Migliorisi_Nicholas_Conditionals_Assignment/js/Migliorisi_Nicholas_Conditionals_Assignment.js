d0aa0fb436d12fb0404effe0dab34719e5cdc794
NMigliorisi <migz517@fullsail.edu>
2015-08-19 21:25:39 -0400

    adding all variables

--- Migliorisi_Nicholas_Conditionals_Assignment/js/Migliorisi_Nicholas_Conditionals_Assignment.js
+++ Migliorisi_Nicholas_Conditionals_Assignment/js/Migliorisi_Nicholas_Conditionals_Assignment.js
@@ -1,2 +1,7 @@
 //Nicholas Migliorisi Conditionals Assignment SDI August 2015 WDDBS
 
+var dayOne = prompt("We are doing to see on average how many hours of sleep people get over the weekend. \nFor this study we are including Friday and Monday.  How many hours of sleep did you get on Friday night?"); //Explaining the study and gathering input for the first night of sleep
+var dayTwo = prompt("How about Saturday night?");//Gathering input for night two
+var dayThree = prompt("And Sunday night?");//Gathering input for night three
+var dayFour = prompt("And finally Monday night?");//Gathering input for night four
+var forgotDay = prompt("Woah there, you forgot one! If you got no sleep one night please type in a zero.");//Gathering any forgotten information
