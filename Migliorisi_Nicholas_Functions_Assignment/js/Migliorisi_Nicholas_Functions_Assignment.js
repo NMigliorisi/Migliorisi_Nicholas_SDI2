032d6db58c79d8fad4265db905e405f9bdf718e1
NMigliorisi <migz517@fullsail.edu>
2015-08-26 11:44:51 -0400

    Creating the while loop for game selection

--- Migliorisi_Nicholas_Functions_Assignment/js/Migliorisi_Nicholas_Functions_Assignment.js
+++ Migliorisi_Nicholas_Functions_Assignment/js/Migliorisi_Nicholas_Functions_Assignment.js
@@ -1,3 +1,18 @@
+//Nicholas Migliorisi  Functions Assignment SDI August 2015  WDDBS
 
+while(true){                                           //Beginning a loop to collect the game type
+    var gameType = prompt("What game do you want to play?\nFlorida State Lottery or Powerball?"); //Asking what game they want to play
+    if(gameType == "Powerball"){                       //Starting an if else statement
+        console.log("Powerball");                      //Printing Powerball to the console if selected game
+        break;                                         //Stopping the loop if Powerball is selected
+    }else{                                             //If Powerball isn't selected we see if it was FS Lotto
+        if(gameType == "Florida State Lottery"){       //Starting another if statement
+            console.log("Florida State Lottery");      //Printing Florida State Lottery to the console if selected game
+            break;                                     //Breaking the loop if FS Lotto is selected
+        }
 
-alert("test");
+    }
+    if(gameType != "Powerball"||"Florida State Lottery"){ //If neither the Powerball or FS Lotto are chosen
+        alert("That is not a valid game! Please try again."); //We alert the user of their error and the loop continues
+    }
+}
